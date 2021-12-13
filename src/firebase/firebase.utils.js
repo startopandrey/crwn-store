27176ed560b4de7deb04c16f310bdf2a840a5035
startopandrey <startopandrey@gmail.com>
2021-12-13 18:19:14 +0200

    implememted sign in with email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+
 const config = {
   apiKey: 'AIzaSyAWfeO3t2UhNGjV5wHlH0Xj0DOw77nq9c4',
   authDomain: 'crwn-db-e583f.firebaseapp.com',
@@ -9,11 +10,39 @@ const config = {
   messagingSenderId: '1061801035517',
   appId: '1:1061801035517:web:d54e80ca7ee23763812094',
 };
-firebase.initializeApp(config);
-export const auth = firebase.auth();
 
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
 export default firebase;
